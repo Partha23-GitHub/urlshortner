86b72a5e700faca677fd94bdcf22514f2715c1e7
ParthaManna <manna79partha@gmail.com>
2022-03-31 11:49:56 +0530

    logic and template design added

--- urlapp/urls.py
+++ urlapp/urls.py
@@ -1,6 +1,6 @@
-from django import views
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.index,name='basepage')
+    path('',views.index,name='basepage'),
+    path('urlshort/',views.urlShotrener,name='urlshortner'),
 ]
--- urlapp/views.py
+++ urlapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render,redirect
+import pyshorteners as ps
 
 # Create your views here.
 def index(request):
     return render(request,'urlapp/index.html')
+
+def urlShotrener(request):
+    if request.method=='POST':
+        input_url=request.POST['input-url']
+        short_url=ps.Shortener().tinyurl.short(input_url)
+        return render(request,'urlapp/index.html',{'key':short_url})
+    else:
+        return render(request,'urlapp/index.html',{'key':'Please try again'})
